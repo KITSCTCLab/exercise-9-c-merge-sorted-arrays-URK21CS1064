1be15e7de2a98a1890c7792ff30a1e8655e0a054
Pratheesh URK21CS1064 <109951975+URK21CS1064@users.noreply.github.com>
2022-10-03 15:57:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,22 @@
 from typing import List
 def merge(a,m,b,n) :
-    
     mp=[]
-    for i in range(m+1):
-        if (m>i):
-            mp.append(a[i])
-        else:
-            for j in range(n):
-                if (n>j):
-                    mp.append(b[j])
+    if a[0]<b[0]:
+        for i in range(m+1):
+            if (m>i):
+                mp.append(a[i])
+            else:
+                for j in range(n):
+                    if (n>j):
+                        mp.append(b[j])
+    else:
+        for i in range(n+1):
+            if (n>i):
+                mp.append(b[i])
+            else:
+                for j in range(m):
+                    if (m>j):
+                        mp.append(a[j])
     print(mp)
         
 nums1 = []
