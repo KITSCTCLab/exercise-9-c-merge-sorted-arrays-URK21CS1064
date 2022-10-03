81c90d085acafe87498ac816140bcc5f4ffdc1b9
Pratheesh URK21CS1064 <109951975+URK21CS1064@users.noreply.github.com>
2022-10-03 15:08:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,22 @@
 from typing import List
-
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
-
-
+import bisect
+def merge(a,m,b,n) :
+    
+    mp=[]
+    for i in range(m):
+        bisect.insort(mp, a[i])
+    for i in range(n):
+        bisect.insort(mp, b[i])
+    for i in mp:
+        print(i,end=' ')
+        
 # Do not change the following code
 nums1 = []
 nums2 = []
 for item in input().split(', '):
-  nums1.append(int(item))
+    nums1.append(int(item))
 for item in input().split(', '):
-  nums2.append(int(item))
+    nums2.append(int(item))
 m = int(input())
 n = int(input())
 merge(nums1, m, nums2, n)
-print(nums1)
