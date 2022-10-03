0a2c447f71024ce7c71c9887d10e6bdb8851dd8f
Pratheesh URK21CS1064 <109951975+URK21CS1064@users.noreply.github.com>
2022-10-03 16:40:18 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,37 +1,63 @@
+from typing import List
 
-def merge(a,m,b,n) :
-    mp=[]
-    if m>n:
-        ch=m
-    else:
-        ch=n
-    flag=0
-    if a[0]<b[0]:
-        for i in range(ch+1):
-            if (m>i):
-                mp.append(a[i])
-            elif (flag==0):
-                for j in range(n):
-                    if (n>j):
-                        mp.append(b[j])
-                        flag=1
-    else:
-        for i in range(ch+1):
-            if (n>i):
-                mp.append(b[i])
-            elif (flag==0):
-                for j in range(m):
-                    if (m>j):
-                        mp.append(a[j])
-                        flag=1
-    print(mp)
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+ 
+  x = nums1[0:m]
+  y = nums2[0:n]
+  nums1=x+y
+  nums1 = merge_sort(nums1)
+  return nums1
+  
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
         
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+  return myList
+
+
+# Do not change the following code
 nums1 = []
 nums2 = []
 for item in input().split(', '):
-    nums1.append(int(item))
+  nums1.append(int(item))
 for item in input().split(', '):
-    nums2.append(int(item))
+  nums2.append(int(item))
 m = int(input())
 n = int(input())
-merge(nums1, m, nums2, n)
+
+print(merge(nums1, m, nums2, n))
